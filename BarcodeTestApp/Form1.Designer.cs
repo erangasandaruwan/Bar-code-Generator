038562b1656fc8b3c1ee89ca221aacdd8d0e924c
erangasandaruwan <eranga361881@hotmail.com>
2018-08-13 15:22:46 +0530

    Add files via upload

---
+++ BarcodeTestApp/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace BarcodeTestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelBarcode = new System.Windows.Forms.Panel();
+            this.txtBarcodeText = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btnSaveImage = new System.Windows.Forms.Button();
+            this.saveFileDlg = new System.Windows.Forms.SaveFileDialog();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.prntDlg = new System.Windows.Forms.PrintDialog();
+            this.SuspendLayout();
+            // 
+            // panelBarcode
+            // 
+            this.panelBarcode.Location = new System.Drawing.Point(13, 120);
+            this.panelBarcode.Name = "panelBarcode";
+            this.panelBarcode.Size = new System.Drawing.Size(356, 149);
+            this.panelBarcode.TabIndex = 0;
+            // 
+            // txtBarcodeText
+            // 
+            this.txtBarcodeText.Location = new System.Drawing.Point(13, 89);
+            this.txtBarcodeText.Name = "txtBarcodeText";
+            this.txtBarcodeText.Size = new System.Drawing.Size(275, 20);
+            this.txtBarcodeText.TabIndex = 1;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(294, 88);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 3;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoEllipsis = true;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(360, 38);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Barcode generation with \r\nTBarCode (11.7.3) SDK for Microsoft® Windows";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(13, 51);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(121, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://www.tec-it.com/";
+            // 
+            // btnSaveImage
+            // 
+            this.btnSaveImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSaveImage.Location = new System.Drawing.Point(12, 281);
+            this.btnSaveImage.Name = "btnSaveImage";
+            this.btnSaveImage.Size = new System.Drawing.Size(137, 23);
+            this.btnSaveImage.TabIndex = 6;
+            this.btnSaveImage.Text = "Save as Image...";
+            this.btnSaveImage.UseVisualStyleBackColor = true;
+            this.btnSaveImage.Click += new System.EventHandler(this.btnSaveImage_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(287, 281);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(82, 23);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPrint.Location = new System.Drawing.Point(153, 281);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(130, 23);
+            this.btnPrint.TabIndex = 8;
+            this.btnPrint.Text = "Print...";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // prntDlg
+            // 
+            this.prntDlg.UseEXDialog = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 309);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSaveImage);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtBarcodeText);
+            this.Controls.Add(this.panelBarcode);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Generate Barcodes with TBarCode";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBarcode;
+        private System.Windows.Forms.TextBox txtBarcodeText;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button btnSaveImage;
+        private System.Windows.Forms.SaveFileDialog saveFileDlg;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.PrintDialog prntDlg;
+    }
+}
+
---
+++ BarcodeTestApp/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TECIT.TBarCode;
+
+namespace BarcodeTestApp
+{
+    public partial class Form1 : Form
+    {
+        private TECIT.TBarCode.Windows.BarcodeControl brcodeController;
+        public Form1()
+        {
+            InitializeComponent();
+
+            saveFileDlg.AddExtension = true;
+            saveFileDlg.Filter =
+              "Bitmap (*.BMP)|*.bmp"
+              + "|CompuServe GIF (*.GIF)|*.gif"
+              + "|Enhanced Metafile (*.EMF)|*.emf"
+              + "|JPEG (*.JPG)|*.jpg"
+              + "|PCX (*.PCX)|*.pcx"
+              + "|PNG (*.PNG)|*.png"
+              + "|TIFF (*.TIF)|*.tif";
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                brcodeController = new TECIT.TBarCode.Windows.BarcodeControl();
+                brcodeController.Anchor = ((AnchorStyles)(((AnchorStyles.Top | AnchorStyles.Left) | AnchorStyles.Right)));
+                brcodeController.Barcode.BearerBarType = TECIT.TBarCode.BearerBarType.None;
+                brcodeController.Barcode.CodablockF.RowSeparatorHeight = -1F;
+                brcodeController.Barcode.Data = txtBarcodeText.Text;
+                brcodeController.Barcode.ModuleWidth = -1F;
+                brcodeController.Location = new Point(0, 3);
+                brcodeController.Name = "barcodeControl1";
+                brcodeController.Size = new Size(300, 100);
+                brcodeController.TabIndex = 0;
+
+                panelBarcode.Controls.Clear();
+                panelBarcode.Controls.Add(brcodeController);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Generate Barcodes with TBarCode",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSaveImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (brcodeController == null)
+                {
+                    MessageBox.Show("Generate Barcodes first before print", "Generate Barcodes with TBarCode", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                DialogResult result = saveFileDlg.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    string filename = saveFileDlg.FileName;
+                    string extension = System.IO.Path.GetExtension(filename);
+                    ImageType type;
+                    switch (extension.ToUpper())
+                    {
+                        case ".BMP":
+                            type = ImageType.Bmp;
+                            break;
+                        case ".GIF":
+                            type = ImageType.Gif;
+                            break;
+                        case ".EMF":
+                            type = ImageType.Emf;
+                            break;
+                        case ".JPG":
+                            type = ImageType.Jpg;
+                            break;
+                        case ".PCX":
+                            type = ImageType.Pcx;
+                            break;
+                        case ".PNG":
+                            type = ImageType.Png;
+                            break;
+                        case ".TIF":
+                            type = ImageType.Tif;
+                            break;
+                        default:
+                            MessageBox.Show("The selected bitmap format is not supported.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                    }
+
+                    brcodeController.Barcode.Draw(filename, type);
+                    MessageBox.Show("Barcode image saved successfully", "Generate Barcodes with TBarCode", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Generate Barcodes with TBarCode", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (brcodeController == null)
+                {
+                    MessageBox.Show("Generate Barcodes first before print", "Generate Barcodes with TBarCode", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                prntDlg.Document = new PrintDocument();
+                DialogResult result = prntDlg.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    prntDlg.Document.PrinterSettings = prntDlg.PrinterSettings;
+                    prntDlg.Document.PrintPage += PrintPage;
+                    prntDlg.Document.Print();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Generate Barcodes with TBarCode", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void PrintPage(object sender, PrintPageEventArgs ev)
+        {
+            try
+            {
+                float dpiX = ev.Graphics.DpiX;
+                float dpiY = ev.Graphics.DpiY;
+                brcodeController.Barcode.BoundingRectangle = new Rectangle((int)(10 / 25.4 * dpiX),
+                                                           (int)(10 / 25.4 * dpiY),
+                                                           (int)(50 / 25.4 * dpiX),
+                                                           (int)(30 / 25.4 * dpiY));
+
+                brcodeController.Barcode.Draw(ev.Graphics);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Generate Barcodes with TBarCode", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
